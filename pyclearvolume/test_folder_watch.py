3793b19bf826518790aa27a87b01716437f10524
Martin Weigert <mweigert@mpi-cbg.de>
2015-02-12 16:27:33 +0100

    minor

--- pyclearvolume/test_folder_watch.py
+++ pyclearvolume/test_folder_watch.py
@@ -12,24 +12,28 @@ class SlowlyFileWriteThread(threading.Thread):
     """
     simulates a slowly write of an image file 
     """
-    def __init__(self,fName, time_to_write = 10., chunksize = 64):
+    def __init__(self,dirName, time_to_write = 10., chunksize = 64):
         super(SlowlyFileWriteThread,self).__init__()
-        self.fName = fName
+        self.dirName = dirName
         self.timestep = 0.1
         self.chunksize = chunksize
         self.nsteps = int(np.ceil(time_to_write/self.timestep))
-        #erase file
-        with open(self.fName,"w") as f:
-            pass
-        
         self.setDaemon(True)
     
 
     def run(self):
-        for i in range(self.nsteps):
-            with open(self.fName,"a") as f:
-                f.write("1"*self.chunksize)                
-            time.sleep(self.timestep)
+        """write constantly files to dir"""
+        while True:
+            fName = os.path.join(self.dirName,"RANDOM_%d.tif"%(time.time()))
+            print "writing to %s\n"%fName
+            #erase file
+            with open(fName,"w") as f:
+                pass
+            for i in range(self.nsteps):
+                with open(fName,"a") as f:
+                    f.write("1"*self.chunksize)                
+                time.sleep(self.timestep)
+            time.sleep(.5)
 
 
 
@@ -37,10 +41,9 @@ if __name__ == "__main__":
 
     dirName =  "tests_watch/"
 
-    randName = "RANDOM_%d.tif"%(time.time())
 
     
-    writeSimulator = SlowlyFileWriteThread(os.path.join(dirName,randName))
+    writeSimulator = SlowlyFileWriteThread(os.path.join(dirName))
 
     writeSimulator.start()
 
